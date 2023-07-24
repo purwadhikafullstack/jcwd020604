940e40c860dc8362a2cfce1c309083b2b91c91b2
Sand <novrialsandi@gmail.com>
2023-07-24 22:43:37 +0700

    feat: MWE-22 init stock

--- projects/client/src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import Test from "../components/test";
-
-export default function HomePage() {
-	return (
-		<>
-			<Test />
-		</>
-	);
-}
--- projects/client/src/routes/Routes.jsx
+++ projects/client/src/routes/Routes.jsx
@@ -1,12 +1,11 @@
 import { Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
 import CollectionPage from "../pages/user/ProductCollectionPage";
 import DetailPage from "../pages/user/ProductDetailPage";
 import AdminManageDataPage from "../pages/admin/AdminManageDataPage";
 import AdminProductPage from "../pages/admin/AdminProductPage";
 
 const routes = [
-	<Route path="/" element={<HomePage />}></Route>,
+	//user
 	<Route path="/collection" element={<CollectionPage />}></Route>,
 	<Route path="/collection/:uuid" element={<DetailPage />}></Route>,
 
