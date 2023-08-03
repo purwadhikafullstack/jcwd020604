728979b097289a83b6bda355c0c10c37f4adcbb1
Prekksu <novrialsandi@gmail.com>
2023-08-03 11:17:42 +0700

    update: MWE-23 init controller

--- projects/server/src/controllers/stockMutationController.js
+++ projects/server/src/controllers/stockMutationController.js
@@ -4,13 +4,16 @@ const Joi = require("joi");
 const moment = require("moment");
 
 const stockMutation = {
-	getStockMutation: async () => {
+	getStockMutation: async (req, res) => {
 		try {
+			const mutation = await db.stock_mutations.findAll();
+
+			return res.status(200).send(mutation);
 		} catch (err) {
 			res.status(500).send({ message: err.message });
 		}
 	},
-	requestStockMutation: async () => {
+	requestStockMutation: async (req, res) => {
 		try {
 		} catch (err) {
 			res.status(500).send({ message: err.message });
