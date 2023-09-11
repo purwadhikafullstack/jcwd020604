3d8d95ad76fa28ba536b40ec98b7e8bb78d4919b
Sand <novrialsandi@gmail.com>
2023-09-11 19:21:11 +0700

    save

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -48,39 +48,39 @@ app.use("/api/report", router.salesReportRouter);
 
 app.use("/api/userImg", express.static(`${__dirname}/public/userImg`));
 app.use(
-  "/api/paymentProof",
-  express.static(`${__dirname}/public/paymentProof`)
+	"/api/paymentProof",
+	express.static(`${__dirname}/public/paymentProof`)
 );
 
 app.get("/api", (req, res) => {
-  res.send(`Hello, this is my API`);
+	res.send(`Hello, this is my API`);
 });
 
 app.get("/api/greetings", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello, Student !",
-  });
+	res.status(200).json({
+		message: "Hello, Student !",
+	});
 });
 
 // ===========================
 
 // not found
 app.use((req, res, next) => {
-  if (req.path.includes("/api/")) {
-    res.status(404).send("Not found !");
-  } else {
-    next();
-  }
+	if (req.path.includes("/api/")) {
+		res.status(404).send("Not found !");
+	} else {
+		next();
+	}
 });
 
 // error
 app.use((err, req, res, next) => {
-  if (req.path.includes("/api/")) {
-    console.error("Error : ", err.stack);
-    res.status(500).send("Error !");
-  } else {
-    next();
-  }
+	if (req.path.includes("/api/")) {
+		console.error("Error : ", err.stack);
+		res.status(500).send("Error !");
+	} else {
+		next();
+	}
 });
 
 //#endregion
@@ -91,15 +91,15 @@ app.use(express.static(join(__dirname, clientPath)));
 
 // Serve the HTML page
 app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, clientPath, "index.html"));
+	res.sendFile(join(__dirname, clientPath, "index.html"));
 });
 
 //#endregion
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log(`ERROR: ${err}`);
-  } else {
-    console.log(`APP RUNNING at ${PORT} ✅`);
-  }
+	if (err) {
+		console.log(`ERROR: ${err}`);
+	} else {
+		console.log(`APP RUNNING at ${PORT} ✅`);
+	}
 });
